447df9f1b7d8b77636cc36f267414c76d7f3eb7c
Sajad Ghadiri <cjanthony.romeo@gmail.com>
2022-05-23 22:08:58 +0430

    i defined 8 class in sub_ingredient.h

--- include/sub_ingredients.h
+++ include/sub_ingredients.h
@@ -12,12 +12,79 @@ public:
     virtual std::string get_name() {return this->name;}
 };
 
-class Cinnamon
+class Chocolate
 {
 public:
     Cinnamon(size_t units) : Ingredient{5, units}
     {
-        this->name = "Cinnamon";
+        this->name = "Chocolate";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+class Sugar
+{
+public:
+    Cinnamon(size_t units) : Ingredient{1, units}
+    {
+        this->name = "Sugar";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+class Cookie
+{
+public:
+    Cinnamon(size_t units) : Ingredient{10, units}
+    {
+        this->name = "Cookie";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+class Espresso
+{
+public:
+    Cinnamon(size_t units) : Ingredient{15, units}
+    {
+        this->name = "Espresso";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+class Milk
+{
+public:
+    Cinnamon(size_t units) : Ingredient{10, units}
+    {
+        this->name = "Milk";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+
+class MilkFoam
+{
+public:
+    Cinnamon(size_t units) : Ingredient{5, units}
+    {
+        this->name = "MilkFoam";
+    }
+
+    virtual std::string get_name() {return this->name;}
+};
+
+class Water
+{
+public:
+    Cinnamon(size_t units) : Ingredient{1, units}
+    {
+        this->name = "Water";
     }
 
     virtual std::string get_name() {return this->name;}
--- src/espresso_based.cpp
+++ src/espresso_based.cpp
@@ -1,7 +1,15 @@
 #include "espresso_based.h"
+#include "sub_ingredients"
 
 class EspressoBased
 {
-    
+    std::string EspressoBased::get_name()
+    {
+        return name ;
+    }
 
+    double EspressoBased::price()
+    {
+        
+    }
 }
